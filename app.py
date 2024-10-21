ab2aa38628774b0a719dc0858aa96184cc546c3c
kumarkse <abhishektiwari2100@gmail.com>
2024-10-21 18:13:08 +0530

    added functionality to test midi via sign in button

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , send_from_directory 
+import os
 
 app = Flask(__name__)
+
+FILE_DIRECTORY = os.path.join(app.root_path,'static','audio')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(FILE_DIRECTORY, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
